c1716fed9d62a190dfb797372f740302de602a45
CJLlorente99 <61738420+CJLlorente99@users.noreply.github.com>
2020-05-14 16:42:45 +0200

    Add files via upload

--- Iris/main_iris.c
+++ Iris/main_iris.c
@@ -12,6 +12,7 @@ static char xck[20] = "XCK";
 int observer();
 int socket_init();
 int iris_init(TipoIris* iris, TipoFlags* flags);
+int ready (int fd);
 
 void *
 total_iris_control (void* ignore)
@@ -20,14 +21,6 @@ total_iris_control (void* ignore)
 
     socket_init();
     iris_init(&iris, &flags);
-
-    /*char* message = "hola\n";
-    int result;
-    result = write(iris.socket_desc, message,strlen(message)); //no consigo comunicacion
-    printf("%d\n", result);
-    fflush(stdout);
-    printf("He pasado\n");
-    fflush(stdout);*/
 
     struct timeval next_activation;
     struct timeval now, timeout;
@@ -40,26 +33,12 @@ total_iris_control (void* ignore)
       timeval_sub (&timeout, &next_activation, &now);
       select (0, NULL, NULL, NULL, &timeout);
 
+      if(ready(iris.socket_desc)){
+          observer();
+      }
+
       fsm_fire (iris_fsm);
     }
-}
-
-void *
-socket_receive_observer(void* ignore) { //funcion que se encarga de activar los flags en funcion de lo que haya llegado
-
-  struct timeval next_activation;
-  struct timeval now, timeout;
-
-  gettimeofday (&next_activation, NULL);
-  while (1) {
-    struct timeval *period = task_get_period (pthread_self());
-    timeval_add (&next_activation, &next_activation, period);
-    gettimeofday (&now, NULL);
-    timeval_sub (&timeout, &next_activation, &now);
-    select (0, NULL, NULL, NULL, &timeout) ;
-
-    observer();
-  }
 }
 
 int
@@ -68,11 +47,9 @@ main ()
     pthread_t tid1 = task_new ("iris", total_iris_control, CLK_MS, CLK_MS, 1, 2048);
     pthread_t tid2 = task_new ("button_onoff", button_onoff_interruption, CLK_MS, CLK_MS, 2, 2048);
     pthread_t tid3 = task_new ("button_MAQnow", button_MAQnow_interruption, CLK_MS, CLK_MS, 1, 2048);
-    pthread_t tid4 = task_new ("observer", socket_receive_observer, CLK_MS, CLK_MS, 1, 2048);
     pthread_join (tid1, NULL);
     pthread_join (tid2, NULL);
     pthread_join (tid3, NULL);
-    pthread_join (tid4, NULL);
     return 0;
 }
 
@@ -164,4 +141,16 @@ int kbhit(){
   }
 
   return 0;
+}
+
+int ready (int fd){
+  struct timeval timeout = {1,0};
+  fd_set rdset;
+  int res;
+
+  if ( fd < 0 ) return 0;
+  FD_ZERO(&rdset);
+  FD_SET(fd, &rdset);
+  res = select(fd+1, &rdset, NULL, NULL, &timeout);
+  return res > 0;
 }
